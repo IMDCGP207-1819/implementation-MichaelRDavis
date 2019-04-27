0810706e48afbd520e37f384f5dcf963bdf495f1
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-04-27 23:24:16 +0100

    Added a InputComponent object.

---
+++ Engine/Source/Runtime/GameObject/Components/InputComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "GameObject/Component.h"
+
+/**
+ * 
+ */
+class InputComponent : public Component
+{
+public:
+	/** Default constructor. */
+	InputComponent();
+
+	/** Default destructor. */
+	~InputComponent();
+};
