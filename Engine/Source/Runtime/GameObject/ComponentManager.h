239c7f1e82fc92347307da571ea0236689496536
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-04-28 22:41:55 +0100

    Added a ComponentManager

---
+++ Engine/Source/Runtime/GameObject/ComponentManager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/**
+ * Container class for managing components.
+ */
+class ComponentManager
+{
+public:
+	/** Default constructor. */
+	ComponentManager();
+
+	/** Default destructor. */
+	~ComponentManager();
+
+	typedef uint32_t ComponentID;
+
+private:
+	/**  */
+};
