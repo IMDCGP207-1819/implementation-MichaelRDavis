6a2bad420d417a13803a5b695573dd76f2f9a0e5
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-05-15 16:42:57 +0100

    Added comments to string utility class.

--- Engine/Source/Runtime/String/String.h
+++ Engine/Source/Runtime/String/String.h
@@ -1,12 +1,12 @@
 #pragma once
 
 /**
- * 
+ * String utility class.
  */
 class String
 {
 public:
-	/**  */
+	/** Normalize a given string. */
 	static std::string Normalize(std::string& str)
 	{
 		std::transform(str.begin(), str.end(), str.begin(), ::tolower);
